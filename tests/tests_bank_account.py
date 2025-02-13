577081e68c0610557d34114ec9cf7eac5b7a615b
Moises Ariza <arizamoises@gmail.com>
2025-02-13 05:57:29 -0500

    Add TearDown functionality to tests

--- tests/tests_bank_account.py
+++ tests/tests_bank_account.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, os
 
 from src.bank_account import BankAccount
 
@@ -7,7 +7,16 @@ class BankAccountTests(unittest.TestCase):
     #Queda pendiente una tarea
     #El método setup se ejecuta antes de cualquier prueba y se puede usar para centralizar el código repetido y compartido entre las funciones de prueba
     def setUp(self):
-        self.account = BankAccount(balance=1000)
+        self.account = BankAccount(balance=1000, log_file="transaction_log.txt")
+
+    #Método que se ejecuta al finalizar las pruebas
+    def tearDown(self):
+        if os.path.exists(self.account.log_file):
+            os.remove(self.account.log_file)
+
+    def _count_lines(self, filename):
+        with open(filename, "r") as f:
+            return len(f.readlines())
 
     def test_deposit(self):
 
@@ -36,3 +45,12 @@ class BankAccountTests(unittest.TestCase):
         amount_send = 500
 
         assert self.account.transfer(current_balance, amount_send, destination_account) == "No se puede realizar la trasnferencia saldo insuficiente"
+
+    def test_transaction_log(self):
+        self.account.deposit(500)
+        assert os.path.exists("transaction_log.txt")
+
+    def test_count_transactions(self):
+        assert self._count_lines(self.account.log_file) == 1
+        self.account.deposit(500)
+        assert self._count_lines(self.account.log_file) == 2
